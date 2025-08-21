00831d871513cb69757926fcb228f1ccbd53ed44
Eduarda Velho <email@eduardavelho.com>
2025-08-21 20:40:31 -0300

    init

--- server.js
+++ server.js
@@ -82,8 +82,8 @@ que são os dobros de [5, 6, 7, 8, 9, 10]
 É OBRIGATÓRIO UTILIZAR A FUNÇÃO MAP
 */
 app.get("/dobros/:inicio/:fim", (req, res) => {
-  const inicio = req.params.inicio;
-  const fim = req.params.fim;
+  const inicio = +req.params.inicio;
+  const fim = +req.params.fim;
   const dobros = []; // Dobros aqui;
   res.render("dobros", { dobros: dobros.join(", ") });
 });
